a488e6aca65c33fdd5b5560b6bb49f555dfdf91c
Likith Sai <likithsai481999@gmail.com>
2025-01-04 11:58:55 +0530

    Initial commit

---
+++ user.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+apt update
+apt install -y apache2
+
+# Get the instance ID using the instance metadata
+INSTANCE_ID=$(curl -s http://169.254.169.254/latest/meta-data/instance-id)
+
+# Install the AWS CLI
+apt install -y awscli
+
+cat <<EOF > /var/www/html/index.html
+<!DOCTYPE html>
+<html>
+<head>
+  <title>My Portfolio</title>
+</head>
+<body>
+  <h1>Terraform Project Server 1</h1>
+  <h2>Instance ID: <span style="color:green">$INSTANCE_ID</span></h2>
+  <p>Welcome to Terraform</p>
+  
+</body>
+</html>
+EOF
+
+# Start Apache and enable it on boot
+systemctl start apache2
+systemctl enable apache2
